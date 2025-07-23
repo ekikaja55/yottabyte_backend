a33e3007bc393bae4d14d40a2e0b4fd175bf9225
unknown <rizkiarkant55@gmail.com>
2025-07-23 16:05:34 +0700

    test2

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const io = new Server(server, {
   }
 })
 
-const PORT = process.env.PORT 
+const PORT = 4000 
 
 // === IN-MEMORY ROOM STATE ===
 const roomState = {} // { [roomId]: { action: 'play', timestamp: 42.5, updatedAt: 123456789 } }
